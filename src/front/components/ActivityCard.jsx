a2b4ad4f6e18739e937766ff497a41be35757285
JorgeArevalo-hub <jorgearevalomoran@gmail.com>
2025-06-09 20:00:35 +0000

    Feature: Added Activity view

--- src/front/components/ActivityCard.jsx
+++ src/front/components/ActivityCard.jsx
@@ -1,13 +1,34 @@
-import { Component } from "react"
 import React from "react"
 
-export const ActivityCard = () => {
+export const ActivityCard = (props) => {
 
-    return(
-        <div className="card">
 
+
+
+
+
+
+    return (
+
+        <div>
+            <div className="card cardFormat p-0 rounded">
+
+                <img src={props.img} className="rounded-top-1"></img>
+
+                <h5>{props.title}</h5>
+
+                <p>{props.origin}</p>
+
+                <p>{props.description}</p>
+
+                <p>{props.timeleft}</p>
+
+            </div>
         </div>
-        
 
-    );
+    )
+
+
+
+
 }
--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
+import { Activities } from "./pages/Activities";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 
@@ -23,6 +24,7 @@ export const router = createBrowserRouter(
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
+        <Route path= "/activities" element={<Activities />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
       </Route>
